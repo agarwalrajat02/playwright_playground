2609dffbc37606459f067b5c899a4d9b72fc7e92
Rajat Agarwal <agarwalrajat01@live.com>
2025-08-27 22:49:40 +0530

    environment chanegs

--- utils/environment.js
+++ utils/environment.js
@@ -1,6 +1,9 @@
-// utils/environment.js
-export const configs = {
-  qa: { BASE_URL: 'https://qa1.rupyy.in/' },
+const CURRENT = process.env.ENV || 'prod';
+
+const configs = {
+  qa:      { BASE_URL: 'https://qa1.rupyy.in/' },
   staging: { BASE_URL: 'https://staging.rupyy.in/' },
-  prod: { BASE_URL: 'https://www.rupyy.com' },
+  prod:    { BASE_URL: 'https://www.rupyy.com' }
 };
+
+export default configs[CURRENT];
